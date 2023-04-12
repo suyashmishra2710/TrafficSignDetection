9811e8aaf5b0753dee0c3e47e68b36a7c702be49
Suyashm1027 <mishr100@purdue.edu>
2023-04-12 11:31:05 -0700

    Merged All changes

--- CollisionPrevention.py
+++ CollisionPrevention.py
@@ -3,7 +3,10 @@ sys.path.append('/Users/suyash/miniconda3/lib/python3.9/site-packages')
 import time 
 import pygame
 import random 
-import threading 
+import threading
+import subprocess
+import os
+import re 
 
 #necessary constants and offsets
 vel = 5
@@ -11,8 +14,8 @@ collisionCheckDist = 150 #the number of x,y units that will be checked. Making t
 upDownOffset = 1
 leftRightOffset = 1
 
-#speeds = {'car': 2.25, 'bus': 1.8, 'truck': 1.8, 'bike':2.5} #average speed of vehicles 
-speeds = {'car': 1, 'bus': 1, 'truck': 1, 'bike':1} #average speed of vehicles 
+speeds = {'car': 2.25, 'bus': 1.8, 'truck': 1.8, 'bike':2.5} #average speed of vehicles 
+#speeds = {'car': 1, 'bus': 1, 'truck': 1, 'bike':1} #average speed of vehicles 
 x = {'right':[0,0,0], 'down':[755,727,697], 'left':[1400,1400,1400], 'up':[602,627,657]}
 y = {'right':[348,370,398], 'down':[0,0,0], 'left':[498,466,436], 'up':[800,800,800]}
 vehicles = {'right': {0:[], 1:[], 2:[], 'crossed':0}, 'down': {0:[], 1:[], 2:[], 'crossed':0}, 'left': {0:[], 1:[], 2:[], 'crossed':0}, 'up': {0:[], 1:[], 2:[], 'crossed':0}}
@@ -27,11 +30,88 @@ driverAssigned = False # represents whether there is one car responsible for loo
 simulation_over = 0 # 0 for when simulation is still running, 1 for when simulation is over 
 collisionCount = 0 # counts the number of collisions in trials 
 totalTrials = 0 # counts the total number of simulation trials (so that we can add percentage of trials that have a collison )
-
+lastDirection = 999999
 
 #initializing pygame
 pygame.init()
 simulation = []
+
+#-------------- added from merging the simulation code -------------------------------------#
+def get_model_predictions():
+    os.chdir('./yolov5/img')
+    files_output = (subprocess.run('ls', capture_output=True)).stdout.decode().strip()
+    filenames = files_output.split('\n')
+    os.chdir('..')
+    os.chdir('..')
+    f = open("classes.txt", "w")
+    os.chdir('./yolov5')
+    for imageName in filenames:
+        print(imageName)
+        output = str(subprocess.run(f'python3 detect.py --weights best.pt --source img/{imageName}', shell=True, capture_output=True, text=True))
+        print(output)
+        indices = re.search(r"\d+x\d+ ", output)
+        start = indices.span()[1]
+        indices = re.search(r", \d+.\dms", output)
+        end = indices.span()[0]
+        classes = output[start:end]
+        classList = classes.split(', ')
+        trafficSign = classList[-1]
+        print(classes)
+        print(trafficSign)
+        print("-----")
+        f.write(f'{imageName} {trafficSign}\n')
+    os.chdir('..')
+    return 
+
+# this function reads through the classes.txt file in order to figure out 
+# what test image file name will get rendered and what the neural network 
+#predicts it to be 
+def displayTestImage():
+    with open('./classes.txt', 'r') as tests:
+        k = random.randint(1, 4)
+        for i in range(0, k):
+            line = tests.readline()
+            line = line.split()
+    filePath = f'./yolov5/img/{line[0]}' # extract the image by using the property the first part until the space will be an image file
+    if line[1] == '(no':
+        return filePath, 'Not Detected'
+    else:
+        return filePath, line[2]
+
+
+def renderTestSign(screen):
+    testSign = pygame.image.load(imgPath)
+    testSign = pygame.transform.scale(testSign, (100, 100))
+    screen.blit(testSign, (800, 225))
+
+# this function checks if the "loading zone" of the particular direction the vehicle is moving in is not occupied 
+def checkZones(direction_number):
+    # want to know all the vehicles that are present in the simulation so zone checking can be done 
+    veh_list = []
+    veh_rectlist = []
+    for vehicle in simulation:
+        veh_list.append(vehicle)
+        veh_rectlist.append(vehicle.vehicleRect)
+        # check the rightbox zone
+    if direction_number == 0:
+        rightRect = pygame.Rect(0, 340, 100, 90)
+        zoneCheck = rightRect.collidelist(veh_rectlist)
+        # check the downbox zone 
+    elif direction_number == 1:
+        downRect = pygame.Rect(680, 0, 90, 100)
+        zoneCheck = downRect.collidelist(veh_rectlist)
+        #check the leftbox zone 
+    elif direction_number == 2:
+        leftRect = pygame.Rect(1310, 340, 100, 90)
+        zoneCheck = leftRect.collidelist(veh_rectlist)
+        # check the up box zone 
+    elif direction_number == 3:  
+        upRect = pygame.Rect((590, 700, 90, 100))
+        zoneCheck = upRect.collidelist(veh_rectlist)
+    if zoneCheck != -1:
+        return False 
+    return True 
+#------------------------------------- end of first set of merging simulation code ----------------------------------
 
 class Vehicle(pygame.sprite.Sprite):
     def __init__(self, lane, vehicleType, direction_number, direction):
@@ -159,7 +239,7 @@ class Vehicle(pygame.sprite.Sprite):
     
         
     #updating the coordinates of each vehicle so that simulation has moving vehicles
-    def move(self):
+    def move(self, imgPath, pred):
         if(self.direction == 'right'):
             self.x += self.speed #update the position of the vehicles that are moving right to keep moving right
             self.vehicleRect.right += self.speed    
@@ -167,16 +247,24 @@ class Vehicle(pygame.sprite.Sprite):
             self.x -= self.speed #update the position of the vehicles that are moving left to keep moving left 
             self.vehicleRect.right -= self.speed 
         elif(self.direction == 'down'):
-            #self.stopResponse()   
-            #self.yieldResponse()
-            #self.speedUpResponse()
-            #self.slowDownResponse()
-            #self.normalResponse()
-            self.laneChangeResponse()
+            if pred == 'stop':
+                self.stopResponse()  
+            elif pred == 'yield': 
+                self.yieldResponse()
+            elif pred == 'speed up':
+                self.speedUpResponse()
+            elif pred == 'speed down':
+                self.slowDownResponse()
+            elif pred == 'merge':
+                self.laneChangeResponse()
+            else:
+                self.normalResponse()
         elif(self.direction == 'up'):
             self.y -= self.speed #update position of vehicles moving up to continue to move up 
             self.vehicleRect.bottom -= self.speed
         if self.x <0 or self.x > 1400 or self.y < 0 or self.y > 800:
+            if self.direction == 'down':
+                imgPath, pred = displayTestImage() 
             simulation.remove(self)
         #self.collisionDetection() #This should do nothing if the collision detections work
         for vehicle1 in simulation:
@@ -191,6 +279,8 @@ class Vehicle(pygame.sprite.Sprite):
                     self.checkRight(vehicle1.vehicleRect, vehicle2.vehicleRect)
                 else:
                     self.checkLeft(vehicle1.vehicleRect, vehicle2.vehicleRect)
+        self.collisionDetection()
+        return imgPath, pred
 
 
                 
@@ -286,6 +376,7 @@ class Vehicle(pygame.sprite.Sprite):
 class Main:
     def generateVehicles():
         while(True):
+            global lastDirection
             carDown = False
             vehicle_type = random.randint(0,3) # the random vehicle created
             lane_number = random.randint(1,2) # the lane in particular direction 
@@ -311,17 +402,30 @@ class Main:
                     break
             #print(f'Found a vehicle in simulation going downwards {carDown}')
             # render all vehicles if they aren't a car and only render cars if there isn't one currently going downwards 
+            '''
             if vehicle_type != 0:
                 Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number])
             elif not carDown:
                 Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number])
             time.sleep(0.05)
             break
+            '''
+            zoneFree = checkZones(direction_number)
+            if vehicle_type != 0 and direction_number != lastDirection and zoneFree: 
+                Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number])
+                lastDirection = direction_number
+            elif not carDown and direction_number != lastDirection and zoneFree:
+                Vehicle(lane_number, vehicleTypes[vehicle_type], direction_number, directionNumbers[direction_number])
+                lastDirection = direction_number
+            time.sleep(0.01)
+    
+    #get_model_predictions()
+    imgPath, pred = displayTestImage() #opens up the classes.txt file and returns the file path of the test image to display and prediction from neural network model
     
     # the driving force behind coordinating the display of our simulation 
-    #thread2 = threading.Thread(name="generateVehicles",target=generateVehicles, args=())
-    #thread2.daemon = True
-    #thread2.start()
+    thread2 = threading.Thread(name="generateVehicles",target=generateVehicles, args=())
+    thread2.daemon = True
+    thread2.start()
     
     black = (0, 0, 0)
     white = (255, 255, 255) 
@@ -329,15 +433,17 @@ class Main:
     screenHeight = 800
     screenSize = (screenWidth, screenHeight)
     background = pygame.image.load('images/intersection.png')
+    testSign = pygame.image.load(imgPath)
+    testSign = pygame.transform.scale(testSign, (100, 100))
     screen = pygame.display.set_mode(screenSize)
     pygame.display.set_caption("TRAFFIC SIMULATION")
     global simulation_over
     
     '''
-        # the driving force behind coordinating the display of our simulation 
-        thread2 = threading.Thread(name="generateVehicles",target=generateVehicles, args=())
-        thread2.daemon = True
-        thread2.start()
+    # the driving force behind coordinating the display of our simulation 
+    thread2 = threading.Thread(name="generateVehicles",target=generateVehicles, args=())
+    thread2.daemon = True
+    thread2.start()
     '''
     
     firstRun = 0 #to easily be able to add object collision detection 
@@ -347,12 +453,31 @@ class Main:
     #simulation.append(v1)
     #simulation.append(v2)
     while True:
-        generateVehicles()
+        #generateVehicles()
+        # for the coordinates of the zones to check if there's a vehicle in before rendering it in and needing to prevent a collision 
+        # the third argument is in (x, y, width, height) format 
+        pygame.draw.rect(screen, (255, 0, 0), (680, 0, 90, 100), 5) # downbox
+        pygame.draw.rect(screen, (255, 0, 0), (0, 340, 100, 90), 5) # rightbox 
+        pygame.draw.rect(screen, (255,0,0), (1310, 340, 100, 90), 5) # leftbox
+        pygame.draw.rect(screen, (255,0,0), (590, 700, 90, 100), 5) #upbox
+        #pygame.draw.line(screen, (255, 0, 0), (x1, y1), (x2, y2), 2)
+        #pygame.draw.line(screen, (255,0,0), (1400, 340), (1400, 430), 5)
+        pygame.display.update()
+        testSign = pygame.image.load(imgPath)
+        testSign = pygame.transform.scale(testSign, (100, 100))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     sys.exit()
-        
-        if simulation_over == 3:
+        if simulation_over == 0:
+            screen.blit(background, (0, 0)) # renders the entire intersection onto the screen
+            screen.blit(testSign, (800, 225)) # renders the specific test image onto the screen for the car to read 
+            for vehicle in simulation: 
+                screen.blit(vehicle.image, vehicle.vehicleRect)
+                #pygame.draw.line(screen, pygame.Color('black'), (700, 350), (900, 350)) # as you approach 350 want to stop 
+                if vehicle.direction_number == 1:
+                    pygame.draw.rect(screen, (255,0,0), vehicle.vehicleRect, 4)
+                imgPath, pred = vehicle.move(imgPath, pred)
+        elif simulation_over == 3:
             green = (255, 255, 255)
             blue = (0, 0, 0)
             X = 2200
@@ -363,14 +488,6 @@ class Main:
             txtRect.center = (X // 2, Y // 2)
             screen.blit(txt, txtRect)
             simulation_over = 3
-        if simulation_over == 0:
-            screen.blit(background, (0, 0))
-            for vehicle in simulation: 
-                screen.blit(vehicle.image, vehicle.vehicleRect)
-                #pygame.draw.line(screen, pygame.Color('black'), (700, 350), (900, 350)) # as you approach 350 want to stop 
-                if vehicle.direction_number == 1:
-                    pygame.draw.rect(screen, (255,0,0), vehicle.vehicleRect, 4)
-                vehicle.move()
         else: 
             green = (255, 255, 255)
             blue = (0, 0, 0)
